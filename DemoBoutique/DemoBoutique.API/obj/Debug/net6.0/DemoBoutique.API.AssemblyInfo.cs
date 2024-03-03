e99142343332822e6af318e16733ed84dee78d30
mourad-bouraq <mourad.bouraq@neo-techit.ma>
2024-03-04 00:33:44 +0100

    update

--- DemoBoutique/DemoBoutique.API/obj/Debug/net6.0/DemoBoutique.API.AssemblyInfo.cs
+++ DemoBoutique/DemoBoutique.API/obj/Debug/net6.0/DemoBoutique.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoBoutique.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8549601c940835e174dba45232260f66a6ffecf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoBoutique.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoBoutique.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
