11f7a10434613dac309a8dffc05418c6c26335c8
mourad-bouraq <mourad.bouraq@neo-techit.ma>
2024-03-07 21:11:43 +0100

    add produit

--- DemoBoutique/DemoBoutique.Infrastructure/Migrations/20240306221556_migration.Designer.cs
+++ DemoBoutique/DemoBoutique.Infrastructure/Migrations/20240306221556_migration.Designer.cs
@@ -4,6 +4,7 @@ using DemoBoutique.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DemoBoutique.Infrastructure.Migrations
 {
     [DbContext(typeof(DbContextDemoBoutique))]
-    partial class DbContextDemoBoutiqueModelSnapshot : ModelSnapshot
+    [Migration("20240306221556_migration")]
+    partial class migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
